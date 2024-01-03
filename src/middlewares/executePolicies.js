59a3f2f8475b33576ea772033e63342402a4e698
Julian Paredes <julian.paredes.am1@gmail.com>
2024-01-03 17:26:52 -0300

    Fix execute policies

--- src/middlewares/executePolicies.js
+++ src/middlewares/executePolicies.js
@@ -6,8 +6,9 @@ const executePolicies = (policies) =>{
         if(policies[0]==="NO_AUTH"&&req.user) return res.sendUnauthorized("Already Logged In");
         if(policies[0]==="AUTH"&&req.user) return next();
         if(policies[0]==="AUTH"&&!req.user) return res.sendUnauthorized('Not logged');
+        if (!req.user) return res.sendUnauthorized('Not logged');
         if(!policies.includes(req.user.role.toUpperCase())){
-            res.sendForbidden('Cannot access');
+            return res.sendForbidden('Cannot access');
         }
         next();
     }
